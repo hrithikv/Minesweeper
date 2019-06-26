d8b3ae3a8dc66f55edb1cdd22563084adb0b454c
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-26 10:22:05 -0400

    Update screen.js

--- screen.js
+++ screen.js
@@ -1,44 +1,44 @@
 export class Board {
   constructor(total_rows, total_columns, total_bombs) {
     this._total_bombs = total_bombs;
-    this._numberOfTiles = total_rows*total_columns;
+    this._total_tiles = total_rows*total_columns;
     this._player_screen = Board.generateplayer_screen(total_rows, total_columns);
     this._bomb_screen = Board.generatebomb_screen(total_rows, total_columns, total_bombs);
   };
   get player_screen() {
     return this._player_screen;
   };
-  run_maze(row_index, columnIndex) {
-    if (this._player_screen[row_index][columnIndex] !== ' '){
+  run_maze(row_index, column_index) {
+    if (this._player_screen[row_index][column_index] !== ' '){
       console.log("This tile has already been flipped!");
       return;
     }
-    else if(this._bomb_screen[row_index][columnIndex] === 'B') {
-      this._player_screen[row_index][columnIndex] = 'B';
+    else if(this._bomb_screen[row_index][column_index] === 'B') {
+      this._player_screen[row_index][column_index] = 'B';
     }
     else {
-      this._player_screen[row_index][columnIndex] = this.find_neighbor_bombs(row_index, columnIndex);
+      this._player_screen[row_index][column_index] = this.find_neighbor_bombs(row_index, column_index);
     }
-    this._numberOfTiles--;
+    this._total_tiles--;
   };
-  getNumberOfNeighborBombs(row_index, columnIndex){
+  getNumberOfNeighborBombs(row_index, column_index){
     const neighborOffsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1. -1], [1, 0], [1, 1]];
     const total_rows = this._bomb_screen.length;
     const total_columns = this._bomb_screen[0].length;
     let total_bombs = 0;
     neighborOffsets.forEach(offset => {
       const neighbor_row_index = row_index + offset[0];
-      const neighborColumnIndex = columnIndex + offset[1];
-      if (neighbor_row_index >= 0 && neighbor_row_index < total_rows && neighborColumnIndex >= 0 && neighborColumnIndex < total_columns) {
-        if (this._bomb_screen[neighbor_row_index][neighborColumnIndex] === "B") {
+      const neighbor_column_index = column_index + offset[1];
+      if (neighbor_row_index >= 0 && neighbor_row_index < total_rows && neighbor_column_index >= 0 && neighbor_column_index < total_columns) {
+        if (this._bomb_screen[neighbor_row_index][neighbor_column_index] === "B") {
           total_bombs++;
         }
       }
     });
     return total_bombs
     };
-    hasSafeTiles() {
-    return this._numberOfTiles !== this._total_bombs;
+    has_available_paths() {
+    return this._total_tiles !== this._total_bombs;
     };
     print() {
       console.log(this._player_screen.map(row => row.join('|')).join('\n'));
@@ -73,9 +73,9 @@ export class Board {
       while(curr_bombs_count < total_bombs)
       {
         let random_row_index = Math.floor(Math.random() * total_rows);
-        let randomColumnIndex = Math.floor(Math.random() * total_columns);
-        if (board[random_row_index][randomColumnIndex] !== 'B') {
-          board[random_row_index][randomColumnIndex] = 'B';
+        let random_column_index = Math.floor(Math.random() * total_columns);
+        if (board[random_row_index][random_column_index] !== 'B') {
+          board[random_row_index][random_column_index] = 'B';
           curr_bombs_count++;
         }
        }
